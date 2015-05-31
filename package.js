e33423236fac16fc3418199a2a8bcca2f61bbee9
SungYongJang <jsy@adain.kr>
2015-05-30 20:13:05 -0700

    add export

--- package.js
+++ package.js
@@ -6,11 +6,12 @@
 Package.describe({
   name: 'adain:classjs',
   summary: 'Class JS https://classjs.readthedocs.org/en/latest/index.html',
-  version: '1.0.0',
+  version: '1.0.2',
   git: 'https://github.com/ADAIN/meteor-classjs.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.addFiles('classjs.js', ['client', 'server']);
+  api.export('Class', ['client', 'server']);
 });
